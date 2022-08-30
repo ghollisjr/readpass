7f0290fcce6dbfe96474ca308e728742beea8098
ghollisjr <ghollisjr@gmail.com>
2022-08-30 16:47:29 -0400

    Removed unnecessary package qualifiers

--- readpass.lisp
+++ readpass.lisp
@@ -7,25 +7,25 @@
 (defun readpass ()
   "Opens TK window to enter password securely, then returns text."
   (let ((result nil))
-    (ltk:with-ltk ()
-      (let* ((main (make-instance 'ltk:frame :name "frame" :master nil))
+    (with-ltk ()
+      (let* ((main (make-instance 'frame :name "frame" :master nil))
              (message
-               (make-instance 'ltk:message
+               (make-instance 'message
                               :master main
                               :text "Enter message:"))
              (text
-               (make-instance 'ltk:entry
+               (make-instance 'entry
                               :master main
                               :show "*"
                               :takefocus t)))
-        (ltk:pack main)
-        (ltk:pack message :side :left)
-        (ltk:pack text :side :right)
-        (ltk:bind text "<Return>"
-                  (lambda (event)
-                    (declare (ignore event))
-                    (setf result
-                          (ltk:text text))
-                    (setf ltk:*exit-mainloop* t)))
-        (ltk:force-focus text)))
+        (pack main)
+        (pack message :side :left)
+        (pack text :side :right)
+        (bind text "<Return>"
+              (lambda (event)
+                (declare (ignore event))
+                (setf result
+                      (text text))
+                (setf *exit-mainloop* t)))
+        (force-focus text)))
     result))
